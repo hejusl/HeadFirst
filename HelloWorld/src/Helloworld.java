fc3c3ad1c6351d90f252f4eccbab87d9cbe02ffa
Henrycc <44864501@qq.com>
2019-03-23 08:01:33 +0800

    java learning project for HeadFirstJava

---
+++ HelloWorld/src/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        double x = Math.random();
+        int y = (int) (x * 13);
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
---
+++ ex01/src/main/java/ex01.java
@@ -0,0 +1,7 @@
+class ex01 {
+    public static void main(String[] args){
+        for (int x = 0; x < 10; x = x + 1) {
+            System.out.println("x is now " + x);
+        }
+    }
+}
